2812b721c63e5a87dc70abaa65f10f7bd693fe52
Alex Kreskiyan <brutalllord@gmail.com>
2014-03-27 03:07:21 +0400

    Error tested

---
+++ src/lang/Error.js
@@ -0,0 +1,74 @@
+/**
+ This file is core of xs.js 0.1
+
+ Copyright (c) 2013-2014, Annium Inc
+
+ Contact:  http://annium.com/contact
+
+ GNU General Public License Usage
+ This file may be used under the terms of the GNU General Public License version 3.0 as
+ published by the Free Software Foundation and appearing in the file LICENSE included in the
+ packaging of this file.
+
+ Please review the following information to ensure the GNU General Public License version 3.0
+ requirements will be met: http://www.gnu.org/copyleft/gpl.html.
+
+ If you are unsure which license is appropriate for your use, please contact the sales department
+ at http://annium.com/contact.
+
+ */
+/**
+ * array class pre-definition
+ * @type {}
+ * @private
+ */
+'use strict';
+(function (root, ns) {
+
+    //framework shorthand
+    var xs = root[ns];
+
+    var error = xs.Error = new (function () {
+        /**
+         * Raises error with optional specified type
+         * @param {String} message
+         * @param {Function} type
+         * @throws {Error|*}
+         */
+        this.raise = function (message, type) {
+            message = message || 'error';
+            type = type || Error;
+            throw new type(message);
+        };
+        /**
+         * Raises reference error
+         * @param message
+         * @throws {ReferenceError}
+         */
+        this.raiseReference = function (message) {
+            this.raise(message, ReferenceError);
+        };
+        /**
+         * Raises reference error
+         * @param message
+         * @throws {ReferenceError}
+         */
+        this.raiseSyntax = function (message) {
+            this.raise(message, SyntaxError);
+        };
+        /**
+         * Raises reference error
+         * @param message
+         * @throws {ReferenceError}
+         */
+        this.raiseType = function (message) {
+            this.raise(message, TypeError);
+        };
+    });
+    xs.Object.extend(xs, {
+        raise: error.raise,
+        raiseReferenceError: error.raiseReference,
+        raiseSyntaxError: error.raiseSyntax,
+        raiseTypeError: error.raiseType
+    });
+})(window, 'xs');
--- tests/lang/Error/test.js
+++ tests/lang/Error/test.js
@@ -9,3 +9,74 @@ function speed(fn, n) {
     console.log('mark: about', n / duration, 'operation per ms');
 }
 module('xs.lang.Error');
+test('raise', function () {
+    function CustomError(message) {
+        this.message = message;
+    }
+
+    throws(function () {
+        xs.Error.raise();
+    }, Error, 'raises error correctly with default message and default type');
+    throws(function () {
+        xs.Error.raise();
+    }, /$/, 'raises error correctly with default message and default type');
+
+    throws(function () {
+        xs.Error.raise('test');
+    }, Error, 'raises error correctly with default type');
+    throws(function () {
+        xs.Error.raise('test');
+    }, /test$/, 'raises error correctly with default type');
+
+    throws(function () {
+        xs.Error.raise('test', CustomError);
+    }, CustomError, 'raises error correctly');
+    throws(function () {
+        xs.Error.raise('test', CustomError);
+    }, /test$/, 'raises error correctly');
+});
+test('raiseReference', function () {
+    throws(function () {
+        xs.Error.raiseReference();
+    }, ReferenceError, 'raises error correctly with default message');
+    throws(function () {
+        xs.Error.raiseReference();
+    }, /$/, 'raises error correctly with default message');
+
+    throws(function () {
+        xs.Error.raiseReference('test');
+    }, ReferenceError, 'raises error correctly');
+    throws(function () {
+        xs.Error.raiseReference('test');
+    }, /test$/, 'raises error correctly');
+});
+test('raiseSyntax', function () {
+    throws(function () {
+        xs.Error.raiseSyntax();
+    }, SyntaxError, 'raises error correctly with default message');
+    throws(function () {
+        xs.Error.raiseSyntax();
+    }, /$/, 'raises error correctly with default message');
+
+    throws(function () {
+        xs.Error.raiseSyntax('test');
+    }, SyntaxError, 'raises error correctly');
+    throws(function () {
+        xs.Error.raiseSyntax('test');
+    }, /test$/, 'raises error correctly');
+});
+test('raiseType', function () {
+    throws(function () {
+        xs.Error.raiseType();
+    }, TypeError, 'raises error correctly with default message');
+    throws(function () {
+        xs.Error.raiseType();
+    }, /$/, 'raises error correctly with default message');
+
+    throws(function () {
+        xs.Error.raiseType('test');
+    }, TypeError, 'raises error correctly');
+    throws(function () {
+        xs.Error.raiseType('test');
+    }, /test$/, 'raises error correctly');
+});
