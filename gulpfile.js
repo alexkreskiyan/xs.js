01f188b2bc8384afd4257536cbd2809d771849ab
Alex <brutalllord@gmail.com>
2014-04-04 14:10:10 +0400

    gulp-uglify added minified version building added

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,48 @@
 var gulp = require('gulp');
 
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 var paths = {
-	scripts: [
-		'src/xs.js',
-		'src/lang/Object.js',
-		'src/lang/Array.js',
-		'src/lang/Set.js',
-		'src/Detect.js',
-		'src/lang/Attribute.js',
-		'src/lang/Date.js',
-		'src/lang/Error.js',
-		'src/lang/Function.js',
-		'src/lang/Number.js',
-		'src/lang/String.js',
-		'src/class/Class.js',
-		'src/class/ClassManager.js',
-		'src/class/Base.js',
-		'src/class/Loader.js',
-		'src/util/Observable.js',
-		'src/promise/Deferred.js',
-		'src/promise/Promise.js',
-		'src/promise/Resolver.js',
-		'src/data/Connection.js',
-		'src/Ajax.js'
-	]
+    scripts: [
+        'src/xs.js',
+        'src/lang/Object.js',
+        'src/lang/Array.js',
+        'src/lang/Set.js',
+        'src/Detect.js',
+        'src/lang/Attribute.js',
+        'src/lang/Date.js',
+        'src/lang/Error.js',
+        'src/lang/Function.js',
+        'src/lang/Number.js',
+        'src/lang/String.js',
+        'src/class/Class.js',
+        'src/class/ClassManager.js',
+        'src/class/Base.js',
+        'src/class/Loader.js',
+        'src/util/Observable.js',
+        'src/promise/Deferred.js',
+        'src/promise/Promise.js',
+        'src/promise/Resolver.js',
+        'src/data/Connection.js',
+        'src/Ajax.js'
+    ]
 };
 
-gulp.task('scripts', function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  return gulp.src(paths.scripts)
-    .pipe(concat('xs.js'))
-    .pipe(gulp.dest('build'));
+gulp.task('build uncompressed', function () {
+    // Minify and copy all JavaScript (except vendor scripts)
+    return gulp.src(paths.scripts)
+        .pipe(concat('xs.js'))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build minified', function () {
+    // Minify and copy all JavaScript (except vendor scripts)
+    return gulp.src(paths.scripts)
+        .pipe(uglify())
+        .pipe(concat('xs.min.js'))
+        .pipe(gulp.dest('build'));
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['scripts']);
+gulp.task('default', ['build uncompressed', 'build minified']);
