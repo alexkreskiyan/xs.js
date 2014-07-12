36e252fec0d6f62701ff3da7fc7a00147160f900
Alex Kreskiyan <brutalllord@gmail.com>
2014-07-13 01:47:56 +0400

    xs.Base: added methods: - clone - destroy - toJSON completely documented and tested

--- src/class/Base.js
+++ src/class/Base.js
@@ -18,69 +18,72 @@
 
  */
 'use strict';
-(function (root, ns) {
-
-    //framework shorthand
-    var xs = root[ns];
-
-    /**
-     * Define xs.Base class
-     */
-    xs.define('xs.Base', {
-        static: {
-            properties: {
-            },
-            methods: {
-                /**
-                 * Returns whether this is child of given parent
-                 * @param parent
-                 * @returns {Boolean}
-                 */
-                isChild: function (parent) {
-                    var me = this;
-                    if (me.parent == parent) {
-                        return true;
-                    } else if (me.parent.isChild) {
-                        return me.parent.isChild(parent);
-                    } else {
-                        return false;
-                    }
-                },
-                /**
-                 * Returns whether this is parent of given child
-                 * @param child
-                 * @returns {Boolean}
-                 */
-                isParent: function (child) {
-                    return xs.isFunction(child.isChild) ? child.isChild(this) : false;
-                }
-            }
-        },
+/**
+ * @class xs.Base
+ * xs.Base is base class for all classes, defined by xs.define. All classes, defined that way, inherit xs.Base.
+ */
+xs.define('xs.Base', {
+    static: {
         methods: {
             /**
-             * returns clone of this object
-             * @return {xs.Base} clone object
+             * Returns whether this is child of given parent
+             * @static
+             * @method
+             * @param {xs.Base} parent
+             * @return {Boolean}
              */
-            clone: function () {
+            isChild: function (parent) {
                 var me = this;
-                return xs.create(me.self.label, me.toJSON());
+                if (me.parent == parent) {
+                    return true;
+                } else if (me.parent.isChild) {
+                    return me.parent.isChild(parent);
+                } else {
+                    return false;
+                }
             },
             /**
-             * common method for complete destructing of object
+             * Returns whether this is parent of given child
+             * @static
+             * @method
+             * @param {xs.Base} child
+             * @return {Boolean}
              */
-            destroy: xs.emptyFn,
-            /**
-             * returns json data of this object
-             * @returns {Object} data hash
-             */
-            toJSON: function () {
-                var me = this,
-                    json = {};
-                xs.Object.each(me.self.descriptor.properties, function (descriptor, name) {
-                    json[name] = me[name];
-                });
-                return json;
+            isParent: function (child) {
+                return xs.isFunction(child.isChild) ? child.isChild(this) : false;
             }
         }
-    });
-})(window, 'xs');
+    },
+    methods: {
+        /**
+         * Returns clone of this object. Basically clone is made by instantiating Class with this object {@link #toJSON JSON representation}
+         * @member
+         * @method
+         * @return {xs.Base} clone object
+         */
+        clone: function () {
+            var me = this;
+            return xs.create(me.self.label, me.toJSON());
+        },
+        /**
+         * Completes internal object destruction
+         * @member
+         * @method
+         */
+        destroy: xs.emptyFn,
+        /**
+         * Returns object JSON representation. Basically is returned hash with object's values for all declared properties
+         * @member
+         * @method
+         * @return {Object} object JSON representation
+         */
+        toJSON: function () {
+            var me = this,
+                json = {};
+            xs.Object.each(me.self.descriptor.properties, function (descriptor, name) {
+                json[name] = me[name];
+            });
+            return json;
+        }
+    }
+});
