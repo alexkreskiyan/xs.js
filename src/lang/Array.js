5c282acf69930979d71cf6e6a3aba9f2e2c877ba
Alex Kreskiyan <brutalllord@gmail.com>
2014-11-21 19:21:08 +0400

    xs.lang.Array styled

--- src/lang/Array.js
+++ src/lang/Array.js
@@ -1,20 +1,11 @@
-/*!
+/*
  This file is core of xs.js
 
  Copyright (c) 2013-2014, Annium Inc
 
- Contact:  http://annium.com/contact
+ Contact: http://annium.com/contact
 
- GNU General Public License Usage
- This file may be used under the terms of the GNU General Public License version 3.0 as
- published by the Free Software Foundation and appearing in the file LICENSE included in the
- packaging of this file.
-
- Please review the following information to ensure the GNU General Public License version 3.0
- requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-
- If you are unsure which license is appropriate for your use, please contact the sales department
- at http://annium.com/contact.
+ License: http://annium.com/contact
 
  */
 (function (root, ns) {
@@ -27,13 +18,13 @@
     /**
      * xs.lang.Array is private singleton, defining basic Array operations.
      *
-     * @class xs.lang.Array
-     *
      * @author Alex Kreskiyan <brutalllord@gmail.com>
      *
-     * @singleton
-     *
      * @private
+     *
+     * @class xs.lang.Array
+     *
+     * @singleton
      */
     var array = xs.Array = new (function () {
         var me = this;
