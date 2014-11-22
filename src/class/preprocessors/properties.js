eabdc70e097dcdbe10f15322e8a0faa3311ef2e8
Alex Kreskiyan <brutalllord@gmail.com>
2014-11-22 18:13:27 +0400

    xs.lang.class.preprocessors styled

--- src/class/preprocessors/properties.js
+++ src/class/preprocessors/properties.js
@@ -1,20 +1,11 @@
-/*!
+/*
  This file is core of xs.js
 
  Copyright (c) 2013-2014, Annium Inc
 
- Contact:  http://annium.com/contact
+ Contact: http://annium.com/contact
 
- GNU General Public License Usage
- This file may be used under the terms of the GNU General Public License version 3.0 as
- published by the Free Software Foundation and appearing in the file LICENSE included in the
- packaging of this file.
-
- Please review the following information to ensure the GNU General Public License version 3.0
- requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-
- If you are unsure which license is appropriate for your use, please contact the sales department
- at http://annium.com/contact.
+ License: http://annium.com/contact
 
  */
 (function (root, ns) {
@@ -31,6 +22,7 @@
      * @author Alex Kreskiyan <brutalllord@gmail.com>
      */
     xs.Class.preprocessors.add('properties', function () {
+
         return true;
     }, function (Class, descriptor) {
 
