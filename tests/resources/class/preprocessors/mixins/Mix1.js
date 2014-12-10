7572b25a76786e89bf51a10b6f10242ccc2ae0c3
Alex <a.kreskiyan@gmail.com>
2014-12-11 01:36:48 +0300

    needed complex chains detection

--- tests/resources/class/preprocessors/mixins/Mix1.js
+++ tests/resources/class/preprocessors/mixins/Mix1.js
@@ -3,6 +3,7 @@
  */
 xs.define('ns.Mix1', function () {
     this.namespace = 'tests.class.preprocessors.mixins';
+    this.imports = ['ns.Mix2'];
     this.constants.a = 1;
     this.properties.a = 2;
     this.methods.printA = function () {
