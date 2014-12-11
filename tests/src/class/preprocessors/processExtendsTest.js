3902309818a22e238313c84d8da58b96c6eb4e73
Alex <a.kreskiyan@gmail.com>
2014-12-12 00:59:52 +0300

    mixins and extends preprocessors' tests splitted

---
+++ tests/src/class/preprocessors/processExtendsTest.js
@@ -0,0 +1,106 @@
+/*
+ This file is core of xs.js
+
+ Copyright (c) 2013-2014, Annium Inc
+
+ Contact: http://annium.com/contact
+
+ License: http://annium.com/contact
+
+ */
+require([
+    'xs.lang.Type',
+    'xs.lang.List',
+    'xs.lang.Object',
+    'xs.lang.Attribute',
+    'xs.lang.Function',
+    'xs.core.Debug',
+    'xs.class.Class',
+    'xs.class.ClassManager',
+    'xs.class.preprocessors.namespace',
+    'xs.class.preprocessors.imports',
+    'xs.class.preprocessors.extends',
+    'xs.class.Base'
+], function () {
+
+    'use strict';
+
+    module('xs.class.preprocessors.extends');
+
+    test('extend base', function () {
+        //create Class
+        var Class = xs.Class.create(function () {
+        });
+
+        //Class extends xs.Base
+        strictEqual(Class.parent, xs.Base);
+    });
+
+    test('extend chain', function () {
+        //setUp
+
+        //Base
+        var BaseName = 'my.Base';
+
+        //define
+        var Base = xs.Class.create(function () {
+        });
+
+        //save
+        var BaseSave = xs.ClassManager.get(BaseName);
+        BaseSave && xs.ClassManager.delete(BaseName);
+
+        //add to ClassManager
+        xs.ClassManager.add(BaseName, Base);
+
+        //Parent
+        var ParentName = 'my.Parent';
+
+        //define
+        var Parent = xs.Class.create(function () {
+            this.extends = 'my.Base';
+        });
+
+        //save
+        var ParentSave = xs.ClassManager.get(ParentName);
+        ParentSave && xs.ClassManager.delete(ParentName);
+
+        //add to ClassManager
+        xs.ClassManager.add(ParentName, Parent);
+
+        //Child
+        var ChildName = 'my.Child';
+
+        //define
+        var Child = xs.Class.create(function () {
+            this.extends = 'my.Parent';
+        });
+
+        //save
+        var ChildSave = xs.ClassManager.get(ChildName);
+        ChildSave && xs.ClassManager.delete(ChildName);
+
+        //add to ClassManager
+        xs.ClassManager.add(ChildName, Child);
+
+        //check chain
+        strictEqual(my.Base.parent, xs.Base);
+        strictEqual(my.Parent.parent, my.Base);
+        strictEqual(my.Child.parent, my.Parent);
+
+        //tearDown
+
+        //Base
+        xs.ClassManager.delete(BaseName);
+        BaseSave && xs.ClassManager.add(BaseName, BaseSave);
+
+        //Parent
+        xs.ClassManager.delete(ParentName);
+        ParentSave && xs.ClassManager.add(ParentName, ParentSave);
+
+        //Child
+        xs.ClassManager.delete(ChildName);
+        ChildSave && xs.ClassManager.add(ChildName, ChildSave);
+    });
+    //TODO test async extend with using xs.Loader
+});
---
+++ tests/src/class/preprocessors/processMixinsTest.js
@@ -0,0 +1,60 @@
+/*
+ This file is core of xs.js
+
+ Copyright (c) 2013-2014, Annium Inc
+
+ Contact: http://annium.com/contact
+
+ License: http://annium.com/contact
+
+ */
+require([
+    'xs.lang.Type',
+    'xs.lang.List',
+    'xs.lang.Object',
+    'xs.lang.Attribute',
+    'xs.lang.Function',
+    'xs.core.Debug',
+    'xs.class.Class',
+    'xs.class.ClassManager',
+    'xs.class.Loader',
+    'xs.class.preprocessors.namespace',
+    'xs.class.preprocessors.prepareExtends',
+    'xs.class.preprocessors.prepareMixins',
+    'xs.class.preprocessors.imports',
+    'xs.class.preprocessors.processExtends',
+    'xs.class.preprocessors.prepareConstants',
+    'xs.class.preprocessors.prepareStaticProperties',
+    'xs.class.preprocessors.prepareStaticMethods',
+    'xs.class.preprocessors.prepareProperties',
+    'xs.class.preprocessors.prepareMethods',
+    'xs.class.preprocessors.processMixins',
+    'xs.class.preprocessors.singleton',
+    'xs.class.preprocessors.defineConstants',
+    'xs.class.preprocessors.defineStaticProperties',
+    'xs.class.preprocessors.defineStaticMethods',
+    'xs.class.preprocessors.constructor',
+    'xs.class.preprocessors.defineProperties',
+    'xs.class.preprocessors.defineMethods',
+    'xs.class.Base'
+], function () {
+
+    'use strict';
+
+    module('xs.class.preprocessors.mixins');
+
+    test('mixins chain', function () {
+        expect(0);
+        xs.Loader.paths.add('tests', '/tests/resources');
+        xs.define('ns.Child', function () {
+            this.namespace = 'tests.class.preprocessors.mixins';
+            this.imports = [
+                'ns.Base',
+                'ns.Mix2'
+            ];
+            this.extends = 'ns.Base';
+            this.mixins.mix2 = 'ns.Mix2';
+        });
+    });
+
+});
