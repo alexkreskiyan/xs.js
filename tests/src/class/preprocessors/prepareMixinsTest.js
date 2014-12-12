242ae52029c266324837ed75e84618529b541214
Alex <a.kreskiyan@gmail.com>
2014-12-13 00:43:56 +0300

    added prepareMixins method

--- tests/src/class/preprocessors/prepareMixinsTest.js
+++ tests/src/class/preprocessors/prepareMixinsTest.js
@@ -21,40 +21,16 @@ require([
     'xs.class.preprocessors.namespace',
     'xs.class.preprocessors.prepareExtends',
     'xs.class.preprocessors.prepareMixins',
-    'xs.class.preprocessors.imports',
-    'xs.class.preprocessors.processExtends',
-    'xs.class.preprocessors.prepareConstants',
-    'xs.class.preprocessors.prepareStaticProperties',
-    'xs.class.preprocessors.prepareStaticMethods',
-    'xs.class.preprocessors.prepareProperties',
-    'xs.class.preprocessors.prepareMethods',
-    'xs.class.preprocessors.processMixins',
-    'xs.class.preprocessors.singleton',
-    'xs.class.preprocessors.defineConstants',
-    'xs.class.preprocessors.defineStaticProperties',
-    'xs.class.preprocessors.defineStaticMethods',
-    'xs.class.preprocessors.constructor',
-    'xs.class.preprocessors.defineProperties',
-    'xs.class.preprocessors.defineMethods',
     'xs.class.Base'
 ], function () {
 
     'use strict';
 
-    module('xs.class.preprocessors.mixins');
+    module('xs.class.preprocessors.prepareMixins');
 
-    test('mixins chain', function () {
+    test('prepareMixins', function () {
         expect(0);
-        xs.Loader.paths.add('tests', '/tests/resources');
-        xs.define('ns.Child', function () {
-            this.namespace = 'tests.class.preprocessors.mixins';
-            this.imports = [
-                'ns.Base',
-                'ns.Mix2'
-            ];
-            this.extends = 'ns.Base';
-            this.mixins.mix2 = 'ns.Mix2';
-        });
+        //TODO
     });
 
 });
