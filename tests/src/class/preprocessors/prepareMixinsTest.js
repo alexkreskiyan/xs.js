e70def366beed09b446ccdb563a27240184e1548
Alex <a.kreskiyan@gmail.com>
2014-12-14 15:45:11 +0300

    xs.class.preprocessors.prepareMixins test updated

--- tests/src/class/preprocessors/prepareMixinsTest.js
+++ tests/src/class/preprocessors/prepareMixinsTest.js
@@ -11,8 +11,37 @@
 module('xs.class.preprocessors.prepareMixins', function () {
 
     test('prepareMixins', function () {
-        expect(0);
-        //TODO
+        var me = this;
+
+        //Class
+        me.ClassName = 'my.Class';
+
+        //define
+        me.Class = xs.Class.create(function () {
+            var me = this;
+            me.mixins.demo = 'xs.Base';
+        });
+
+        //save
+        me.ClassSave = xs.ClassManager.get(me.ClassName);
+        me.ClassSave && xs.ClassManager.delete(me.ClassName);
+
+        //add to ClassManager
+        xs.ClassManager.add(me.ClassName, me.Class);
+
+        xs.onReady(me.done);
+
+        return false;
+    }, function () {
+        //check chain
+        strictEqual(xs.size(my.Class.descriptor.mixins), 1);
+        strictEqual(my.Class.descriptor.mixins.demo, 'xs.Base');
+
+    }, function () {
+        var me = this;
+        //Class
+        xs.ClassManager.delete(me.ClassName);
+        me.ClassSave && xs.ClassManager.add(me.ClassName, me.ClassSave);
     });
 
 });
