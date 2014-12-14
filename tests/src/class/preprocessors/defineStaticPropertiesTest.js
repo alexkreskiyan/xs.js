89f86761c5c1897d5af0460cf8a5be78f7d91596
Alex <a.kreskiyan@gmail.com>
2014-12-15 00:21:35 +0300

    xs.class.preprocessors.defineStaticProperties test updated

--- tests/src/class/preprocessors/defineStaticPropertiesTest.js
+++ tests/src/class/preprocessors/defineStaticPropertiesTest.js
@@ -11,11 +11,13 @@
 module('xs.class.preprocessors.defineStaticProperties', function () {
 
     test('static properties chain', function () {
+        var me = this;
+
         //Base
-        var BaseName = 'my.Base';
+        me.BaseName = 'my.Base';
 
         //define
-        var Base = xs.Class.create(function () {
+        me.Base = xs.Class.create(function () {
             this.static.properties.a = {
                 get: function () {
 
@@ -25,17 +27,17 @@ module('xs.class.preprocessors.defineStaticProperties', function () {
         });
 
         //save
-        var BaseSave = xs.ClassManager.get(BaseName);
-        BaseSave && xs.ClassManager.delete(BaseName);
+        me.BaseSave = xs.ClassManager.get(me.BaseName);
+        me.BaseSave && xs.ClassManager.delete(me.BaseName);
 
         //add to ClassManager
-        xs.ClassManager.add(BaseName, Base);
+        xs.ClassManager.add(me.BaseName, me.Base);
 
         //Parent
-        var ParentName = 'my.Parent';
+        me.ParentName = 'my.Parent';
 
         //define
-        var Parent = xs.Class.create(function () {
+        me.Parent = xs.Class.create(function () {
             this.extends = 'my.Base';
             this.static.properties.a = {
                 get: function () {
@@ -52,17 +54,17 @@ module('xs.class.preprocessors.defineStaticProperties', function () {
         });
 
         //save
-        var ParentSave = xs.ClassManager.get(ParentName);
-        ParentSave && xs.ClassManager.delete(ParentName);
+        me.ParentSave = xs.ClassManager.get(me.ParentName);
+        me.ParentSave && xs.ClassManager.delete(me.ParentName);
 
         //add to ClassManager
-        xs.ClassManager.add(ParentName, Parent);
+        xs.ClassManager.add(me.ParentName, me.Parent);
 
         //Child
-        var ChildName = 'my.Child';
+        me.ChildName = 'my.Child';
 
         //define
-        var Child = xs.Class.create(function () {
+        me.Child = xs.Class.create(function () {
             this.extends = 'my.Parent';
             this.static.properties.a = 2;
             this.static.properties.c = {
@@ -78,11 +80,15 @@ module('xs.class.preprocessors.defineStaticProperties', function () {
         });
 
         //save
-        var ChildSave = xs.ClassManager.get(ChildName);
-        ChildSave && xs.ClassManager.delete(ChildName);
+        me.ChildSave = xs.ClassManager.get(me.ChildName);
+        me.ChildSave && xs.ClassManager.delete(me.ChildName);
 
         //add to ClassManager
-        xs.ClassManager.add(ChildName, Child);
+        xs.ClassManager.add(me.ChildName, me.Child);
+
+        xs.onReady(me.done);
+
+        return false;
     }, function () {
         //Base
         //a
@@ -117,16 +123,18 @@ module('xs.class.preprocessors.defineStaticProperties', function () {
         strictEqual(my.Child.c, '?3!');
         strictEqual(my.Child.privates.c, '?3');
     }, function () {
+        var me = this;
+
         //Base
-        xs.ClassManager.delete(BaseName);
-        BaseSave && xs.ClassManager.add(BaseName, BaseSave);
+        xs.ClassManager.delete(me.BaseName);
+        me.BaseSave && xs.ClassManager.add(me.BaseName, me.BaseSave);
 
         //Parent
-        xs.ClassManager.delete(ParentName);
-        ParentSave && xs.ClassManager.add(ParentName, ParentSave);
+        xs.ClassManager.delete(me.ParentName);
+        me.ParentSave && xs.ClassManager.add(me.ParentName, me.ParentSave);
 
         //Child
-        xs.ClassManager.delete(ChildName);
-        ChildSave && xs.ClassManager.add(ChildName, ChildSave);
+        xs.ClassManager.delete(me.ChildName);
+        me.ChildSave && xs.ClassManager.add(me.ChildName, me.ChildSave);
     });
 });
