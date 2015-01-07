f20acab17ad903718dbb328d780db3137fcb571f
Alex <a.kreskiyan@gmail.com>
2015-01-07 15:37:51 +0300

    xs.ux.Promise basics done, tested

--- src/ux/Promise.js
+++ src/ux/Promise.js
@@ -134,15 +134,15 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
     Class.methods.resolve = function (data) {
         var me = this;
 
+        xs.assert.not(me.private.isDestroyed, 'Object is destroyed', PromiseError);
         xs.log(self.label + '::resolve - data:', data);
         xs.assert.equal(me.private.state, self.PENDING, 'Promise is already resolved', PromiseError);
 
+        //set new state
+        me.private.state = self.RESOLVED;
+
         //process promise on next tick
         xs.nextTick(function () {
-
-            //set new state
-            me.private.state = self.RESOLVED;
-
             _processPromise.call(me, 'resolve', data);
         });
     };
@@ -157,31 +157,32 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
     Class.methods.reject = function (reason) {
         var me = this;
 
+        xs.assert.not(me.private.isDestroyed, 'Object is destroyed', PromiseError);
         xs.log(self.label + '::reject - reason:', reason);
         xs.assert.equal(me.private.state, self.PENDING, 'Promise is already rejected', PromiseError);
 
+        //set new state
+        me.private.state = self.REJECTED;
+
         //process promise on next tick
         xs.nextTick(function () {
-
-            //set new state
-            me.private.state = self.REJECTED;
-
             _processPromise.call(me, 'reject', reason);
         });
     };
 
     /**
-     * Promise progress method. Doesn't change promise state, only consequently runs all registered progress handlers.
-     * So, this method could be run many times before promise is resolved or rejected. After that, progress method is prohibited
+     * Promise update method. Doesn't change promise state, only consequently runs all registered progress handlers.
+     * So, this method could be run many times before promise is resolved or rejected. After that, update method is prohibited
      *
-     * @method progress
+     * @method update
      *
      * @param {*} state some value, that means updated state of pending operation, handled by promise
      */
-    Class.methods.progress = function (state) {
+    Class.methods.update = function (state) {
         var me = this;
 
-        xs.log(self.label + '::progress - state:', state);
+        xs.assert.not(me.private.isDestroyed, 'Object is destroyed', PromiseError);
+        xs.log(self.label + '::update - state:', state);
         xs.assert.equal(me.private.state, self.PENDING, 'Promise is already ' + me.private.state, PromiseError);
 
         //process promise on next tick
@@ -189,8 +190,8 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
 
             //process promise handlers
             me.private.handlers.each(function (item) {
-                if (item.progress) {
-                    _handleItem(item, 'progress', state);
+                if (item.update) {
+                    _handleItem(item, 'update', state);
                 }
             });
         });
@@ -203,15 +204,18 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
      *
      * @method then
      *
-     * @param {Function|undefined} handleResolved handler, that will be called, when promise will be resolved. Undefined, if param is omitted
-     * @param {Function|undefined} handleRejected handler, that will be called, when promise will be rejected. Undefined, if param is omitted
-     * @param {Function|undefined} handleProgress handler, that will be called, when promise will change it's progress state, but not yet resolved|rejected. Undefined, if param is omitted
+     * @param {Function|undefined} [handleResolved] handler, that will be called, when promise will be resolved. Undefined, if param is omitted
+     * @param {Function|undefined} [handleRejected] handler, that will be called, when promise will be rejected. Undefined, if param is omitted
+     * @param {Function|undefined} [handleProgress] handler, that will be called, when promise will change it's progress state, but not yet resolved|rejected. Undefined, if param is omitted
+     *
+     * @chainable
      *
      * @return {xs.ux.Promise}
      */
     Class.methods.then = function (handleResolved, handleRejected, handleProgress) {
         var me = this;
 
+        xs.assert.not(me.private.isDestroyed, 'Object is destroyed', PromiseError);
         xs.log(self.label + '::then');
 
         var item = _createItem(handleResolved, handleRejected, handleProgress);
@@ -238,12 +242,63 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
         }
 
 
-        //resolve item immediately
-        _handleItem(item, me.private.state === self.RESOLVED ? 'resolve' : 'reject', me.private.data);
+        //resolve item on next tick
+        xs.nextTick(function () {
+            _handleItem(item, me.private.state === self.RESOLVED ? 'resolve' : 'reject', me.private.data);
+        });
 
         //if promise is pending - add item to handlers
 
         return item.promise;
+    };
+
+    /**
+     * Core promise method. Adds handler for promise future state, when it will be rejected
+     * If no handler given, method does nothing. If any handler given, new promise object will be returned, constructing
+     * chain of promise objects, allowing to perform stack of async operations
+     *
+     * @method otherwise
+     *
+     * @param {Function|undefined} [handleRejected] handler, that will be called, when promise will be rejected. Undefined, if param is omitted
+     *
+     * @chainable
+     *
+     * @return {xs.ux.Promise}
+     */
+    Class.methods.otherwise = function (handleRejected) {
+        return this.then(undefined, handleRejected);
+    };
+
+    /**
+     * Core promise method. Adds handler for promise future state, when its progress will be changed (not rejected/resolved yet)
+     * If no handler given, method does nothing. If any handler given, new promise object will be returned, constructing
+     * chain of promise objects, allowing to perform stack of async operations
+     *
+     * @method progress
+     *
+     * @param {Function|undefined} [handleProgress] handler, that will be called, when promise will change it's progress state, but not yet resolved|rejected. Undefined, if param is omitted
+     *
+     * @chainable
+     *
+     * @return {xs.ux.Promise}
+     */
+    Class.methods.progress = function (handleProgress) {
+        return this.then(undefined, undefined, handleProgress);
+    };
+
+    /**
+     * Promise destroy method
+     *
+     * @method destroy
+     */
+    Class.methods.destroy = function () {
+        var me = this;
+
+        //remove all handlers
+        me.private.handlers.remove();
+
+        //call parent method
+        self.parent.prototype.destroy.call(me);
     };
 
     /**
@@ -271,7 +326,8 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
             }
         });
 
-        me.destroy();
+        //remove all handlers
+        me.private.handlers.remove();
     };
 
     /**
@@ -338,7 +394,7 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
             }, function (reason) {
                 promise.reject(reason);
             }, function (state) {
-                promise.progress(state);
+                promise.update(state);
             });
 
             return;
@@ -370,10 +426,10 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
             $handleResolved: handleResolved
         }, PromiseError);
         xs.assert.ok(!xs.isDefined(handleRejected) || xs.isFunction(handleRejected), 'createItem - given "$handleRejected" is not a function', {
-            $handleResolved: handleRejected
+            $handleRejected: handleRejected
         }, PromiseError);
         xs.assert.ok(!xs.isDefined(handleProgress) || xs.isFunction(handleProgress), 'createItem - given "$handleProgress" is not a function', {
-            $handleResolved: handleProgress
+            $handleProgress: handleProgress
         }, PromiseError);
 
         //if nothing given - return undefined
@@ -401,7 +457,7 @@ xs.define(xs.Class, 'ns.Promise', function (self) {
         //add handleProgress if given
         if (xs.isFunction(handleProgress)) {
             xs.log(self.label + '::createItem - progress handler given');
-            item.progress = handleProgress;
+            item.update = handleProgress;
         }
 
         return item;
--- tests/src/ux/PromiseTest.js
+++ tests/src/ux/PromiseTest.js
@@ -12,66 +12,458 @@ module('xs.ux.Promise', function () {
 
     'use strict';
 
-    test('promise', function () {
-        expect(0);
-
-        var p = new xs.ux.Promise();
-        var state = 0;
-        var step = 25;
-        var max = 100;
-        var interval = setInterval(function () {
-            if (state < max) {
-                state += step;
-                p.progress(state);
-            } else {
-                p.resolve('ready');
-                clearInterval(interval);
-            }
-        }, 1000);
-        p.then(function (data) {
-            console.info('first promise resolved with data:', data, '- item one');
-            var p = new xs.ux.Promise();
-            setTimeout(function () {
-                console.info('internal promise resolved with data:', data, '- item one');
-                p.resolve(data);
-            }, 1000);
-
-            return p;
-        }, function (reason) {
-            console.info('first promise rejected with reason:', reason, '- item one');
-
-            return reason;
-        }, function (state) {
-            console.info('first promise progressed to state:', state, '- item one');
-
-            return state;
-        }).then(function (data) {
-            console.info('second promise resolved with data:', data, '- item one');
-
-            return data;
-        }, function (reason) {
-            console.info('second promise rejected with reason:', reason, '- item one');
-
-            return reason;
-        }, function (state) {
-            console.info('second promise progressed to state:', state, '- item one');
-
-            return state;
+    test('resolve basic', function () {
+        //init test variables
+        var promise;
+        //resolve destroyed throws
+        promise = new xs.ux.Promise();
+        promise.destroy();
+        throws(function () {
+            promise.resolve();
         });
+
+        //resolve not pending throws
+        promise = new xs.ux.Promise();
+        promise.resolve();
+        throws(function () {
+            promise.resolve();
+        });
+    });
+
+    test('resolve simple plain case normal', function () {
+        //simple case - three L1 handlers. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.source = 5;
+        me.promise.then(function (data) {
+            me.source *= data;
+        });
+        me.promise.then(function (data) {
+            me.source -= data;
+        });
+        me.promise.then(function (data) {
+            me.source /= data;
+        });
+    }, function () {
+        var me = this;
+        me.promise.resolve(5);
+        me.promise.then(function () {
+            strictEqual(me.source, 4);
+            me.done();
+        });
+        return false;
+    });
+
+    test('resolve simple plain case exception', function () {
+        //simple case - three L1 handlers. Second L1 handler throws exception
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.source = 5;
+        me.promise.then(function (data) {
+            me.source *= data;
+        });
+        me.promise.then(function () {
+            throw new Error('error');
+        });
+        me.promise.then(function (reason) {
+            me.source += reason;
+        });
+    }, function () {
+        var me = this;
+        me.promise.resolve(5);
+        me.promise.then(function () {
+            strictEqual(me.source, 30);
+            me.done();
+        });
+        return false;
+    });
+
+    test('resolve simple chain case normal', function () {
+        //simple case - promises chain with three steps. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        var value = 5;
+        me.chain = me.promise.then(function (data) {
+            return data * value;
+        }).then(function (data) {
+            return data - value;
+        }).then(function (data) {
+            return data / value;
+        });
+    }, function () {
+        var me = this;
+        me.promise.resolve(5);
+        me.chain.then(function (data) {
+            strictEqual(data, 4);
+            me.done();
+        });
+        return false;
+    });
+
+    test('resolve simple chain case exception', function () {
+        //simple case - promises chain with three steps. Step two throws exception
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        var value = 5;
+        me.chain = me.promise.then(function (data) {
+            return data * value;
+        }).then(function () {
+            throw new Error('error');
+        }).then(function (data) {
+            return data / value;
+        }, function (e) {
+            return value + e.message;
+        });
+    }, function () {
+        var me = this;
+        me.promise.resolve(5);
+        me.chain.then(function () {
+            throw new Error('failed');
+        }, function (reason) {
+            strictEqual(reason, '5error');
+            me.done();
+        });
+        return false;
+    });
+
+    test('resolve complex case normal', function () {
+        //complex case - promises tree 2 levels with 3 handlers each - 12 total. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.total = 0;
+        me.promise.then(function (data) {
+            return data + 2; //7
+        }).then(function (data) {
+            return data * 2; //14
+        }).then(function (data) {
+            me.total += data; //0 + 14 = 14
+        });
+        me.promise.then(function (data) {
+            return data + 3; //8
+        }).then(function (data) {
+            return data / 4; //2
+        }).then(function (data) {
+            me.total *= data; //14 * 2 = 28
+        });
+        me.last = me.promise.then(function (data) {
+            return data - 1; //4
+        }).then(function (data) {
+            return data * 4; //16
+        }).then(function (data) {
+            me.total -= data; //28 - 16 = 12
+        });
+    }, function () {
+        var me = this;
+        me.promise.resolve(5);
+        //handle last chain
+        me.last.then(function () {
+            strictEqual(me.total, 12);
+            me.done();
+        });
+
+        return false;
+    });
+
+    test('resolve complex case exception', function () {
+        //complex case - promises tree 2 levels with 3 handlers each - 12 total. Second L1 handler throws exception
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.total = 0;
+        me.promise.then(function (data) {
+            return data + 2; //7
+        }).then(function (data) {
+            return data * 2; //14
+        }).then(function (data) {
+            me.total += data; //0 + 14 = 14
+        });
+        me.promise.then(function () {
+            throw new Error('reason');
+        }).then(function (data) {
+            return data / 4; //not going come here
+        }, function (e) {
+            return e.message;
+        }).then(function (data) {
+            me.total *= data; //not going to come here
+        }, function (reason) {
+            me.total += reason; //14 + reason = 14reason
+        });
+        me.last = me.promise.then(function (data) {
+            return data - 1; //4
+        }).then(function (data) {
+            return data * 4; //16
+        }).then(function (data) {
+            me.total += data; //14reason + 16 = 14reason16
+        });
+    }, function () {
+        var me = this;
+        me.promise.resolve(5);
+        //handle last chain
+        me.last.then(function () {
+            strictEqual(me.total, '14reason16');
+            me.done();
+        });
+
+        return false;
+    });
+
+    test('reject basic', function () {
+        //init test variables
+        var promise;
+        //reject destroyed throws
+        promise = new xs.ux.Promise();
+        promise.destroy();
+        throws(function () {
+            promise.reject();
+        });
+
+        //resolve not pending throws
+        promise = new xs.ux.Promise();
+        promise.reject();
+        throws(function () {
+            promise.reject();
+        });
+    });
+
+    test('reject simple plain case normal', function () {
+        //simple case - three L1 handlers. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.source = 5;
+        me.promise.otherwise(function (data) {
+            me.source *= data;
+        });
+        me.promise.otherwise(function (data) {
+            me.source -= data;
+        });
+        me.promise.otherwise(function (data) {
+            me.source /= data;
+        });
+    }, function () {
+        var me = this;
+        me.promise.reject(5);
+        me.promise.otherwise(function () {
+            strictEqual(me.source, 4);
+            me.done();
+        });
+        return false;
+    });
+
+    test('reject simple plain case exception', function () {
+        //simple case - three L1 handlers. Second L1 handler throws exception
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.source = 5;
+        me.promise.otherwise(function (data) {
+            me.source *= data;
+        });
+        me.promise.otherwise(function () {
+            throw new Error('error');
+        });
+        me.promise.otherwise(function (reason) {
+            me.source += reason;
+        });
+    }, function () {
+        var me = this;
+        me.promise.reject(5);
+        me.promise.otherwise(function () {
+            strictEqual(me.source, 30);
+            me.done();
+        });
+        return false;
+    });
+
+    test('reject simple chain case normal', function () {
+        //simple case - promises chain with three steps. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        var value = 5;
+        me.chain = me.promise.otherwise(function (data) {
+            return data * value;
+        }).otherwise(function (data) {
+            return data - value;
+        }).otherwise(function (data) {
+            return data / value;
+        });
+    }, function () {
+        var me = this;
+        me.promise.reject(5);
+        me.chain.otherwise(function (data) {
+            strictEqual(data, 4);
+            me.done();
+        });
+        return false;
+    });
+
+    test('reject simple chain case exception', function () {
+        //simple case - promises chain with three steps. Step two throws exception
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        var value = 5;
+        me.chain = me.promise.otherwise(function (data) {
+            return data * value;
+        }).otherwise(function () {
+            throw new Error('error');
+        }).then(function (data) {
+            return data / value;
+        }, function (e) {
+            return value + e.message;
+        });
+    }, function () {
+        var me = this;
+        me.promise.reject(5);
+        me.chain.then(function () {
+            throw new Error('failed');
+        }, function (reason) {
+            strictEqual(reason, '5error');
+            me.done();
+        });
+        return false;
+    });
+
+    test('reject complex case normal', function () {
+        //complex case - promises tree 2 levels with 3 handlers each - 12 total. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.total = 0;
+        me.promise.otherwise(function (data) {
+            return data + 2; //7
+        }).otherwise(function (data) {
+            return data * 2; //14
+        }).otherwise(function (data) {
+            me.total += data; //0 + 14 = 14
+        });
+        me.promise.otherwise(function (data) {
+            return data + 3; //8
+        }).otherwise(function (data) {
+            return data / 4; //2
+        }).otherwise(function (data) {
+            me.total *= data; //14 * 2 = 28
+        });
+        me.last = me.promise.otherwise(function (data) {
+            return data - 1; //4
+        }).otherwise(function (data) {
+            return data * 4; //16
+        }).otherwise(function (data) {
+            me.total -= data; //28 - 16 = 12
+        });
+    }, function () {
+        var me = this;
+        me.promise.reject(5);
+        //handle last chain
+        me.last.otherwise(function () {
+            strictEqual(me.total, 12);
+            me.done();
+        });
+
+        return false;
+    });
+
+    test('reject complex case exception', function () {
+        //complex case - promises tree 2 levels with 3 handlers each - 12 total. Second L1 handler throws exception
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.total = 0;
+        me.promise.otherwise(function (data) {
+            return data + 2; //7
+        }).otherwise(function (data) {
+            return data * 2; //14
+        }).otherwise(function (data) {
+            me.total += data; //0 + 14 = 14
+        });
+        me.promise.otherwise(function () {
+            throw new Error('reason');
+        }).then(function (data) {
+            return data / 4; //not going come here
+        }, function (e) {
+            return e.message;
+        }).then(function (data) {
+            me.total *= data; //not going to come here
+        }, function (reason) {
+            me.total += reason; //14 + reason = 14reason
+        });
+        me.last = me.promise.otherwise(function (data) {
+            return data - 1; //4
+        }).otherwise(function (data) {
+            return data * 4; //16
+        }).otherwise(function (data) {
+            me.total += data; //14reason + 16 = 14reason16
+        });
+    }, function () {
+        var me = this;
+        me.promise.reject(5);
+        //handle last chain
+        me.last.otherwise(function () {
+            strictEqual(me.total, '14reason16');
+            me.done();
+        });
+
+        return false;
+    });
+
+    test('progress complex case', function () {
+        //complex case - promises tree 2 levels with 3 handlers each - 12 total. All resolved
+        var me = this;
+        me.promise = new xs.ux.Promise;
+        me.total = 0;
+        me.promise.progress(function (data) {
+            return data + 2; //4; 3
+        }).progress(function (data) {
+            return data * 2; //8; 6
+        }).progress(function (data) {
+            me.total += data; //0 + 8 = 8; -2 + 6 = 4
+        });
+        me.promise.progress(function (data) {
+            return data + 3; //5; 4
+        }).progress(function (data) {
+            return data / 4; //1.25; 1
+        }).progress(function (data) {
+            me.total *= data; //8 * 1.25 = 10; 4 * 1 = 4
+        });
+        me.last = me.promise.then(xs.emptyFn, undefined, function (data) {
+            return data + 1; //3; 2
+        }).then(xs.emptyFn, undefined, function (data) {
+            return data * 4; //12; 8
+        }).then(xs.emptyFn, undefined, function (data) {
+            me.total -= data; //10 - 12 = -2; 4 - 8 = -4
+        });
+    }, function () {
+        var me = this;
+        var count = 2;
+        var interval = setInterval(function () {
+            if (count === 0) {
+                clearInterval(interval);
+                me.promise.resolve('n');
+
+                return;
+            }
+
+            me.promise.update(count--);
+        }, 10);
+        //handle last chain
+        me.last.then(function () {
+            strictEqual(me.total, -4);
+            me.done();
+        });
+
+        return false;
+    });
+
+    test('immediate', function () {
+        var me = this;
+        var promise = new xs.ux.Promise();
+        promise.resolve(5);
+        //handlers run immediately if promise is already done
         setTimeout(function () {
-            p.then(function (data) {
-                console.info('first promise resolved with data:', data, '- item two');
+            promise.then(function (data) {
+                strictEqual(data, 5);
+                return data * 2;
+            }).then(function (data) {
+                strictEqual(data, 10);
 
-                return data;
-            }, function (reason) {
-                console.info('first promise rejected with reason:', reason, '- item two');
-
-                return reason;
-            }, function (state) {
-                console.info('first promise progressed to state:', state, '- item two');
-
-                return state;
+                me.done();
             });
-        }, 500);
+        }, 10);
+
+        return false;
     });
 });
