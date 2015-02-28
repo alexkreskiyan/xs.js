8e495144973aef19ce0501147c53856244c6d72c
Alex <a.kreskiyan@gmail.com>
2015-02-28 11:50:19 +0300

    Started reactive

---
+++ src/reactive/Stream.js
@@ -0,0 +1,44 @@
+/*
+ This file is core of xs.js
+
+ Copyright (c) 2013-2014, Annium Inc
+
+ Contact: http://annium.com/contact
+
+ License: http://annium.com/contact
+
+ */
+(function (root, ns) {
+
+    'use strict';
+
+    //framework shorthand
+    var xs = root[ns];
+
+    //define xs.reactive
+    if (!xs.reactive) {
+        xs.reactive = {};
+    }
+
+    /**
+     * xs.reactive.Stream is core framework class, that is used for reactive representations
+     *
+     * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
+     *
+     * @private
+     *
+     * @class xs.reactive.Stream
+     */
+
+    /**
+     * xs.reactive.Stream constructor
+     *
+     * @constructor
+     */
+    xs.reactive.Stream = function () {
+        //var me = this;
+    };
+
+
+
+})(window, 'xs');
