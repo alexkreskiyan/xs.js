0a3ab5e29663b753439ad3a97a3ffbfe5f1cc8a6
Alex <a.kreskiyan@gmail.com>
2015-03-04 00:04:11 +0300

    Added stream early prototype

--- src/reactive/Stream.js
+++ src/reactive/Stream.js
@@ -1,44 +1,75 @@
-/*
- This file is core of xs.js
+'use strict';
 
- Copyright (c) 2013-2014, Annium Inc
+var log = new xs.log.Logger('xs.core.Promise');
 
- Contact: http://annium.com/contact
+var assert = new xs.core.Asserter(log, StreamError);
 
- License: http://annium.com/contact
+//define xs.reactive
+if (!xs.reactive) {
+    xs.reactive = {};
+}
 
+/**
+ * xs.reactive.Stream is core framework class, that is used for reactive representations
+ *
+ * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
+ *
+ * @private
+ *
+ * @class xs.reactive.Stream
  */
-(function (root, ns) {
 
-    'use strict';
+/**
+ * xs.reactive.Stream constructor
+ *
+ * @constructor
+ */
+var Stream = xs.reactive.Stream = function (emitter) {
+    var me = this;
 
-    //framework shorthand
-    var xs = root[ns];
+    //assert, that emitter is a function
+    assert.fn(emitter, 'constructor - given emitter `$emitter` is not a function', {
+        $emitter: emitter
+    });
 
-    //define xs.reactive
-    if (!xs.reactive) {
-        xs.reactive = {};
-    }
+    //run emitter with 2 arguments - send, end
+    var handleEnd = emitter(function (data) {
+        //send data in stream
+        me.send(data);
+    }, function () {
 
-    /**
-     * xs.reactive.Stream is core framework class, that is used for reactive representations
-     *
-     * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
-     *
-     * @private
-     *
-     * @class xs.reactive.Stream
-     */
+        //handle end (free resources)
+        handleEnd();
 
-    /**
-     * xs.reactive.Stream constructor
-     *
-     * @constructor
-     */
-    xs.reactive.Stream = function () {
-        //var me = this;
-    };
+        //internal end handler
+        me.end();
+    });
 
+    //assert, that handleEnd is a function
+    assert.fn(handleEnd, 'constructor - given stream end handler `$handler` is not a function', {
+        $handler: handleEnd
+    });
+};
 
+Stream.prototype.send = function (data) {
+    console.log('send', data);
+};
 
-})(window, 'xs');
+Stream.prototype.end = function () {
+    console.log('end');
+};
+
+/**
+ * Internal error class
+ *
+ * @ignore
+ *
+ * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
+ *
+ * @class StreamError
+ */
+function StreamError(message) {
+    this.message = 'xs.reactive.Stream::' + message;
+}
+
+StreamError.prototype = new Error();
