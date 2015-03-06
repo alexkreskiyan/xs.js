7dd3cad1cf32fcc36b1fc93185fd8f194a2be475
Alex <a.kreskiyan@gmail.com>
2015-03-06 09:10:11 +0300

    Upgraded build system

--- make/task/source.js
+++ make/task/source.js
@@ -5,54 +5,14 @@ module.exports = function () {
     var del = require('del');
     var sources = require('../sources');
     var gulp = require('gulp');
-    var indent = require('gulp-indent');
-    var wrap = require('gulp-wrap');
-    var concat = require('gulp-concat');
-    var merge = require('gulp-merge');
 
     //remove old files
     del(['build/source/*.js']);
 
 
-    //get entry stream
-    var entry = gulp.src(sources.entry);
-
-
     //get core stream
-    var core = gulp.src(sources.core);
-
-    //indent all sources
-    core = core.pipe(indent({
-        amount: 4
-    }));
-
-    //wrap all sources in core template
-    core = core.pipe(wrap({
-        src: 'make/template/core'
-    }));
-
-
-    //get build stream
-    var build = merge(entry, core);
-
-    //indent files
-    build = build.pipe(indent({
-        amount: 4
-    }));
-
-    //concat all files
-    build = build.pipe(concat({
-        path: 'xs.js'
-    }, {
-        newLine: '\n\n\n'
-    }));
-
-    //wrap concatenated file in framework template
-    build = build.pipe(wrap({
-        src: 'make/template/framework'
-    }));
-
+    var core = sources.core;
 
     //save build
-    build.pipe(gulp.dest('build/source'));
+    core.pipe(gulp.dest('build/source'));
 };
