66cce1c19472b597daa250320510b84c1c3db91d
Alex <a.kreskiyan@gmail.com>
2015-03-17 08:58:56 +0300

    xs.core.ReadyManager documented

--- src/core/DependenciesManager.js
+++ src/core/DependenciesManager.js
@@ -359,11 +359,41 @@ Dependency.prototype.getLock = function (chain) {
     return lock;
 };
 
+/**
+ * Private internal core class.
+ *
+ * ReadyManager is used to manage external dependencies on contracts
+ *
+ * @ignore
+ *
+ * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
+ *
+ * @private
+ *
+ * @class xs.core.ReadyManager
+ *
+ * @singleton
+ */
 var ReadyManager = (function () {
     var me = {};
 
+    /**
+     * Internal dependencies manager storage
+     *
+     * @property storage
+     *
+     * @type {xs.core.Collection}
+     */
     var storage = new xs.core.Collection();
 
+    /**
+     * Adds new dependency from contracts with given names or simply when all contracts are processed //TODO - integrate with global processing contracts collection
+     *
+     * @method add
+     *
+     * @param {String[]} [waiting] optional array with names of waited contracts
+     * @param {Function} handleReady handler, being called as far as waiting dependencies are resolved
+     */
     me.add = function (waiting, handleReady) {
 
         //assert, that correct params given
@@ -394,6 +424,11 @@ var ReadyManager = (function () {
         //filter waiting
         waiting = waiting.find(function (name) {
 
+            //assert, that name is correct
+            assert.ok(xs.ContractsManager.isName(name), 'Given name `$name` is not a valid contract name', {
+                $name: name
+            });
+
             //if contract is not defined - wait
             if (!xs.ContractsManager.has(name)) {
 
@@ -414,11 +449,18 @@ var ReadyManager = (function () {
         });
     };
 
-    me.remove = function (processed) {
+    /**
+     * Removes processed contract from ready manager
+     *
+     * @method remove
+     *
+     * @param {String|Null} contract removed contract label
+     */
+    me.remove = function (contract) {
 
         //get resolved queue lists. If processed given - with non-null waiting list. If no processed given - with null lists only
         var resolved;
-        if (processed === null) {
+        if (contract === null) {
             resolved = storage.find(function (item) {
                 //item is resolved, if waiting list is null
 
@@ -433,12 +475,12 @@ var ReadyManager = (function () {
                 }
 
                 //return false if item waiting doesn't have processed
-                if (!item.waiting.has(processed)) {
+                if (!item.waiting.has(contract)) {
                     return false;
                 }
 
                 //remove processed from item waiting
-                item.waiting.remove(processed);
+                item.waiting.remove(contract);
 
                 //item is resolved, if waiting is empty
                 return !item.waiting.size;
