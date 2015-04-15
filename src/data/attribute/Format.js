6b833aa578e1df8de98216fa10a3f4cff7d33277
Alex <a.kreskiyan@gmail.com>
2015-04-15 16:34:42 +0300

    Added xs.data.attribute.Format enum, that specifies format, attribute's value can be fetched in

---
+++ src/data/attribute/Format.js
@@ -0,0 +1,16 @@
+/**
+ * Enum, that specifies used attribute formats, when retrieving value
+ *
+ * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
+ *
+ * @abstract
+ *
+ * @private
+ *
+ * @class xs.data.attribute.Format
+ */
+xs.define(xs.Enum, 'ns.Format', {
+    User: 0x1,
+    Store: 0x2,
+    Raw: 0x4
+});
