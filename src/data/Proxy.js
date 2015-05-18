a1a31c1fdcf983abfa7831b9762a0512a74824c6
Alex <a.kreskiyan@gmail.com>
2015-05-18 19:37:36 +0300

    Tested xs.data.Proxy

--- src/data/Proxy.js
+++ src/data/Proxy.js
@@ -31,51 +31,35 @@ xs.define(xs.Class, 'ns.Proxy', function (self, imports) {
     Class.constructor = function (config) {
         var me = this;
 
+        if (!arguments.length) {
+
+            return;
+        }
+
         //assert, that config is given and is an object
         self.assert.object(config, 'constructor - given config `$config` is not an object', {
             $config: config
         });
 
-        //save config
-        me.private.config = config;
-
 
         //process reader
+        if (config.hasOwnProperty('reader')) {
 
-        //assert reader implements IReader || is object
-        self.assert.object(config.reader, 'constructor - given config.reader `$reader` is not an object', {
-            $reader: config.reader
-        });
-
-        //assert, that config.reader implements IReader
-        self.assert.implements(config.reader, imports.IReader, 'constructor - given config.reader`$reader` does not implement `$Interface`', {
-            $reader: config.reader,
-            $Interface: imports.IReader
-        });
-
-        //save reader
-        me.private.reader = config.reader;
-
+            //save reader
+            me.reader = config.reader;
+        }
 
         //process writer
+        if (config.hasOwnProperty('writer')) {
 
-        //assert writer implements IWriter || is object
-        self.assert.object(config.writer, 'constructor - given config.writer `$writer` is not an object', {
-            $writer: config.writer
-        });
-
-        //assert, that config.writer implements IWriter
-        self.assert.implements(config.writer, imports.IWriter, 'constructor - given config.writer`$writer` does not implement `$Interface`', {
-            $writer: config.writer,
-            $Interface: imports.IWriter
-        });
-
-        //save writer
-        me.private.writer = config.writer;
+            //save writer
+            me.writer = config.writer;
+        }
     };
 
     Class.property.reader = {
         set: function (reader) {
+
             //assert, that reader implements IReader
             self.assert.implements(reader, imports.IReader, 'constructor - given config.reader`$reader` does not implement `$Interface`', {
                 $reader: reader,
@@ -88,6 +72,7 @@ xs.define(xs.Class, 'ns.Proxy', function (self, imports) {
 
     Class.property.writer = {
         set: function (writer) {
+
             //assert, that writer implements IWriter
             self.assert.implements(writer, imports.IWriter, 'constructor - given config.writer`$writer` does not implement `$Interface`', {
                 $writer: writer,
--- tests/src/data/Proxy/test.js
+++ tests/src/data/Proxy/test.js
@@ -8,8 +8,109 @@
  License: http://annium.com/contact
 
  */
-module('xs.data.reader.JSON', function () {
+module('xs.data.Proxy', function () {
 
     'use strict';
+
+    test('constructor', function () {
+        var me = this;
+
+        me.Class = xs.Class(function () {
+
+            var Class = this;
+
+            Class.extends = 'xs.data.Proxy';
+        }, me.done);
+
+        return false;
+    }, function () {
+        var me = this;
+
+        var proxy;
+
+        //proxy can be constructed without config
+        proxy = new me.Class();
+        strictEqual(proxy.reader, undefined);
+        strictEqual(proxy.writer, undefined);
+
+        //given config must be an object
+        throws(function () {
+            return new me.Class(null);
+        });
+
+        //reader can be set within config
+        proxy = new me.Class({
+            reader: new xs.data.reader.JSON()
+        });
+        strictEqual(proxy.reader instanceof xs.data.reader.JSON, true);
+        strictEqual(proxy.writer, undefined);
+
+        //writer can be set within config
+        proxy = new me.Class({
+            writer: new xs.data.writer.JSON()
+        });
+        strictEqual(proxy.reader, undefined);
+        strictEqual(proxy.writer instanceof xs.data.writer.JSON, true);
+    });
+
+    test('reader', function () {
+        var me = this;
+
+        me.Class = xs.Class(function () {
+
+            var Class = this;
+
+            Class.extends = 'xs.data.Proxy';
+        }, me.done);
+
+        return false;
+    }, function () {
+        var me = this;
+
+        var proxy = new me.Class();
+
+        //by default reader is undefined
+        strictEqual(proxy.reader, undefined);
+
+        //can not set incorrect instance
+        throws(function () {
+            proxy.reader = new xs.data.writer.JSON();
+        });
+
+        //correct instance is set normally
+        proxy.reader = new xs.data.reader.JSON();
+
+        strictEqual(proxy.reader instanceof xs.data.reader.JSON, true);
+    });
+
+    test('writer', function () {
+        var me = this;
+
+        me.Class = xs.Class(function () {
+
+            var Class = this;
+
+            Class.extends = 'xs.data.Proxy';
+        }, me.done);
+
+        return false;
+    }, function () {
+        var me = this;
+
+        var proxy = new me.Class();
+
+        //by default writer is undefined
+        strictEqual(proxy.writer, undefined);
+
+        //can not set incorrect instance
+        throws(function () {
+            proxy.writer = new xs.data.reader.JSON();
+        });
+
+        //correct instance is set normally
+        proxy.writer = new xs.data.writer.JSON();
+
+        strictEqual(proxy.writer instanceof xs.data.writer.JSON, true);
+    });
 
 });
