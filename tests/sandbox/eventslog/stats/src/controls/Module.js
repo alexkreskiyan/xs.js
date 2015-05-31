682f55106f8782936adb85a7da93ff3f52d08c73
name <email>
2015-05-31 19:38:55 +0300

    added all controls

--- tests/sandbox/eventslog/stats/src/controls/Module.js
+++ tests/sandbox/eventslog/stats/src/controls/Module.js
@@ -32,35 +32,142 @@ xs.define(xs.Class, 'ns.Module', function (self, imports) {
         fillCommon(common);
 
         //add browser group
-        me.container.items.add(new imports.Group({
+        var browser = new imports.Group({
             name: 'browser',
             label: 'Браузер'
-        }));
+        });
+        me.container.items.add(browser);
+        fillBrowser(browser);
 
         //add engine group
-        me.container.items.add(new imports.Group({
+        var engine = new imports.Group({
             name: 'engine',
             label: 'Движок'
-        }));
+        });
+        me.container.items.add(engine);
+        fillEngine(engine);
 
         //add environment group
-        me.container.items.add(new imports.Group({
+        var environment = new imports.Group({
             name: 'environment',
             label: 'Окружение'
-        }));
+        });
+        me.container.items.add(environment);
+        fillEvironment(environment);
+
     };
 
     function fillCommon(group) {
+        //user
         var user = new imports.Field({
             name: 'user',
             label: 'Пользователь'
         });
         group.items.add(user);
+
+        //device
         var device = new imports.Field({
             name: 'device',
             label: 'Устройство'
         });
         group.items.add(device);
+
+        //category
+        var category = new imports.Field({
+            name: 'category',
+            label: 'Категория'
+        });
+        group.items.add(category);
+
+        //name
+        var name = new imports.Field({
+            name: 'name',
+            label: 'Название'
+        });
+        group.items.add(name);
+    }
+
+    function fillBrowser(group) {
+        //name
+        var name = new imports.Field({
+            name: 'name',
+            label: 'Название'
+        });
+        group.items.add(name);
+
+        //version
+        var version = new imports.Field({
+            name: 'version',
+            label: 'Версия'
+        });
+        group.items.add(version);
+
+        //major
+        var major = new imports.Field({
+            name: 'major',
+            label: 'Мажорная версия'
+        });
+        group.items.add(major);
+
+        //minor
+        var minor = new imports.Field({
+            name: 'minor',
+            label: 'Минорная версия'
+        });
+        group.items.add(minor);
+    }
+
+    function fillEngine(group) {
+        //name
+        var name = new imports.Field({
+            name: 'name',
+            label: 'Название'
+        });
+        group.items.add(name);
+
+        //version
+        var version = new imports.Field({
+            name: 'version',
+            label: 'Версия'
+        });
+        group.items.add(version);
+
+        //major
+        var major = new imports.Field({
+            name: 'major',
+            label: 'Мажорная версия'
+        });
+        group.items.add(major);
+
+        //minor
+        var minor = new imports.Field({
+            name: 'minor',
+            label: 'Минорная версия'
+        });
+        group.items.add(minor);
+    }
+
+    function fillEvironment(group) {
+        //os name
+        var name = new imports.Field({
+            name: 'name',
+            label: 'Название'
+        });
+        group.items.add(name);
+
+        //os version
+        var version = new imports.Field({
+            name: 'version',
+            label: 'Версия'
+        });
+        group.items.add(version);
+
+        //os architecture
+        var architecture = new imports.Field({
+            name: 'architecture',
+            label: 'Архитектура'
+        });
+        group.items.add(architecture);
     }
 
 });
