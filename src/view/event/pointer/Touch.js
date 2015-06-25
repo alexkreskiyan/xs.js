7e36ff993db1e3ca3220e570de4114c35fb2fe0b
Alex <a.kreskiyan@gmail.com>
2015-06-25 17:18:29 +0300

    Added xs.view.event.pointer.Touch class for representing touches

--- src/view/event/pointer/Touch.js
+++ src/view/event/pointer/Touch.js
@@ -7,7 +7,7 @@
  *
  * @extends xs.class.Base
  */
-xs.define(xs.Class, 'ns.pointer.Touch', function (self) {
+xs.define(xs.Class, 'ns.pointer.Touch', function (self, imports) {
 
     'use strict';
 
@@ -15,27 +15,126 @@ xs.define(xs.Class, 'ns.pointer.Touch', function (self) {
 
     Class.namespace = 'xs.view.event';
 
+    Class.imports = {
+        Element: 'xs.view.Element'
+    };
+
     Class.constructor = function (touch) {
         var me = this;
 
-        self.assert.ok(touch instanceof );
+        //assert, that touch is a Touch interface instance
+        self.assert.ok(touch instanceof window.Touch, 'constructor - given `$touch` is not an instance of `$Touch`', {
+            $touch: touch,
+            $Touch: window.Touch
+        });
+
+
+        //verify and set properties from touch
+
+        //assert, that touch.identifier is a number
+        self.assert.number(touch.identifier, 'constructor - given touch.identifier `$identifier` is not a number', {
+            $identifier: touch.identifier
+        });
+        me.private.id = touch.identifier;
+
+        //assert, that touch.screenX is a number
+        self.assert.number(touch.screenX, 'constructor - given touch.screenX `$screenX` is not a number', {
+            $screenX: touch.screenX
+        });
+        me.private.screenX = touch.screenX;
+
+        //assert, that touch.screenY is a number
+        self.assert.number(touch.screenY, 'constructor - given touch.screenY `$screenY` is not a number', {
+            $screenY: touch.screenY
+        });
+        me.private.screenY = touch.screenY;
+
+        //assert, that touch.clientX is a number
+        self.assert.number(touch.clientX, 'constructor - given touch.clientX `$clientX` is not a number', {
+            $clientX: touch.clientX
+        });
+        me.private.clientX = touch.clientX;
+
+        //assert, that touch.clientY is a number
+        self.assert.number(touch.clientY, 'constructor - given touch.clientY `$clientY` is not a number', {
+            $clientY: touch.clientY
+        });
+        me.private.clientY = touch.clientY;
+
+        //assert, that touch.target is an Element instance
+        self.assert.ok(touch.target instanceof Element, 'constructor - given touch.target `$target` is not a `$Element` instance', {
+            $target: touch.target,
+            $Element: Element
+        });
+        me.private.target = touch.target;
     };
 
-    Touch.identifier
-    A unique identifier for this Touch object. A given touch (say, by a finger) will have the same identifier for the duration of its movement around the surface. This lets you ensure that you're tracking the same touch all the time. Read only.
+    /**
+     * A unique identifier for this Touch object.
+     * A given touch (say, by a finger) will have the same identifier for the duration of its movement around the surface.
+     * This lets you ensure that you're tracking the same touch all the time. Read only.
+     *
+     * @type {Number}
+     */
+    Class.property.id = {
+        set: xs.noop
+    };
 
-    Touch.screenX
-    The X coordinate of the touch point relative to the left edge of the screen. Read only.
+    /**
+     * The X coordinate of the touch point relative to the left edge of the screen. Read only.
+     *
+     * @type {Number}
+     */
+    Class.property.screenX = {
+        set: xs.noop
+    };
 
-    Touch.screenY
-    The Y coordinate of the touch point relative to the top edge of the screen. Read only.
+    /**
+     * The Y coordinate of the touch point relative to the top edge of the screen. Read only.
+     *
+     * @type {Number}
+     */
+    Class.property.screenY = {
+        set: xs.noop
+    };
 
-    Touch.clientX
-    The X coordinate of the touch point relative to the left edge of the browser viewport, not including any scroll offset. Read only.
+    /**
+     * The X coordinate of the touch point relative to the left edge of the browser viewport, not including any scroll offset. Read only.
+     *
+     * @type {Number}
+     */
+    Class.property.clientX = {
+        set: xs.noop
+    };
 
-    Touch.clientY
-    The Y coordinate of the touch point relative to the top edge of the browser viewport, not including any scroll offset. Read only.
+    /**
+     * The Y coordinate of the touch point relative to the top edge of the browser viewport, not including any scroll offset. Read only.
+     *
+     * @type {Number}
+     */
+    Class.property.clientY = {
+        set: xs.noop
+    };
 
-    Touch.target
-    The Element on which the touch point started when it was first placed on the surface, even if the touch point has since moved outside the interactive area of that element or even been removed from the document. Note that if the target is removed from the document, events will still be targeted at it, and hence won't necessarily bubble up to the window or document anymore. If there's any risk of an element being removed while it is being touched, best practice is to attach the touch listeners directly to the target. Read only.
+    /**
+     * The Element on which the touch point started when it was first placed on the surface,
+     * even if the touch point has since moved outside the interactive area of that element or even been removed from the document.
+     * Note that if the target is removed from the document, events will still be targeted at it,
+     * and hence won't necessarily bubble up to the window or document anymore.
+     *
+     * @type {xs.view.Element}
+     */
+    Class.property.target = {
+        get: function () {
+            var me = this;
+
+            if (me.private.target instanceof Element) {
+                me.private.target = new imports.Element(me.private.target);
+            }
+
+            return me.private.target;
+        },
+        set: xs.noop
+    };
+
 });
