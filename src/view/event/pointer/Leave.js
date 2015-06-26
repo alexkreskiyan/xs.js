62b01828021bb7da3633658f35205bdb63c40724
Alex <a.kreskiyan@gmail.com>
2015-06-26 11:05:23 +0300

    Enter, Leave, Out and Over events extended from common TargetChange abstract class

--- src/view/event/pointer/Leave.js
+++ src/view/event/pointer/Leave.js
@@ -7,7 +7,7 @@
  *
  * @extends xs.class.Base
  */
-xs.define(xs.Class, 'ns.pointer.Leave', function (self, imports) {
+xs.define(xs.Class, 'ns.pointer.Leave', function (self) {
 
     'use strict';
 
@@ -15,12 +15,7 @@ xs.define(xs.Class, 'ns.pointer.Leave', function (self, imports) {
 
     Class.namespace = 'xs.view.event';
 
-    Class.extends = 'ns.pointer.Pointer';
-
-    Class.imports = {
-        Button: 'ns.pointer.Button',
-        Element: 'xs.view.Element'
-    };
+    Class.extends = 'ns.pointer.TargetChange';
 
     Class.static.method.capture = function (element) {
         var capture = {
@@ -60,48 +55,6 @@ xs.define(xs.Class, 'ns.pointer.Leave', function (self, imports) {
 
         //console.log('fire tapEnd event from pointer...');
         me.element.events.emitter.send(xEvent);
-    };
-
-    /**
-     * Event constructor
-     *
-     * @constructor
-     *
-     * @param {Object} event owned event
-     * @param {Object} data evaluated data
-     */
-    Class.constructor = function (event, data) {
-        var me = this;
-
-        //call parent constructor
-        self.parent.call(me, event, data);
-
-
-        //validate and save event fields
-
-        //relatedTarget
-        self.assert.ok(data.relatedTarget instanceof Element, 'constructor - given data.relatedTarget `$currentTarget` is not a `$Element` instance', {
-            $currentTarget: data.relatedTarget,
-            $Element: Element
-        });
-        me.private.relatedTarget = data.relatedTarget;
-    };
-
-    /**
-     * The EventTarget whose EventListeners are currently being processed.
-     * As the event capturing and bubbling occurs this value changes.
-     */
-    Class.property.relatedTarget = {
-        get: function () {
-            var me = this;
-
-            if (me.private.relatedTarget instanceof Element) {
-                me.private.relatedTarget = new imports.Element(me.private.relatedTarget);
-            }
-
-            return me.private.relatedTarget;
-        },
-        set: xs.noop
     };
 
     var getDataFromPointerEvent = function (event) {
---
+++ src/view/event/pointer/TargetChange.js
@@ -0,0 +1,68 @@
+/**
+ * Abstract event class for events, being thrown when pointer changes it's target
+ *
+ * @author Alex Kreskiyan <a.kreskiyan@gmail.com>
+ *
+ * @class xs.view.event.pointer.TargetChange
+ *
+ * @extends xs.class.Base
+ */
+xs.define(xs.Class, 'ns.pointer.TargetChange', function (self, imports) {
+
+    'use strict';
+
+    var Class = this;
+
+    Class.namespace = 'xs.view.event';
+
+    Class.extends = 'ns.pointer.Pointer';
+
+    Class.imports = {
+        Element: 'xs.view.Element'
+    };
+
+    Class.abstract = true;
+
+    /**
+     * Event constructor
+     *
+     * @constructor
+     *
+     * @param {Object} event owned event
+     * @param {Object} data evaluated data
+     */
+    Class.constructor = function (event, data) {
+        var me = this;
+
+        //call parent constructor
+        self.parent.call(me, event, data);
+
+
+        //validate and save event fields
+
+        //relatedTarget
+        self.assert.ok(data.relatedTarget instanceof Element, 'constructor - given data.relatedTarget `$relatedTarget` is not a `$Element` instance', {
+            $relatedTarget: data.relatedTarget,
+            $Element: Element
+        });
+        me.private.relatedTarget = data.relatedTarget;
+    };
+
+    /**
+     * The EventTarget whose EventListeners are currently being processed.
+     * As the event capturing and bubbling occurs this value changes.
+     */
+    Class.property.relatedTarget = {
+        get: function () {
+            var me = this;
+
+            if (me.private.relatedTarget instanceof Element) {
+                me.private.relatedTarget = new imports.Element(me.private.relatedTarget);
+            }
+
+            return me.private.relatedTarget;
+        },
+        set: xs.noop
+    };
+
+});
