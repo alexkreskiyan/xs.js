f6cb8ac42d493e171153b422a1cb21a8dc8249b4
Alex Kreskiyan <a.kreskiyan@gmail.com>
2015-06-26 09:16:13 +0300

    added xs.view.event.pointer.Leave

--- tests/sandbox/events/tests/src/module/suite/tests/xs/pointer/Test.js
+++ tests/sandbox/events/tests/src/module/suite/tests/xs/pointer/Test.js
@@ -11,6 +11,7 @@ xs.define(xs.Class, 'ns.tests.xs.pointer.Test', function (self, imports) {
             ContextMenu: 'ns.tests.xs.pointer.stage.ContextMenu',
             DoubleTap: 'ns.tests.xs.pointer.stage.DoubleTap',
             Enter: 'ns.tests.xs.pointer.stage.Enter',
+            Leave: 'ns.tests.xs.pointer.stage.Leave',
             Move: 'ns.tests.xs.pointer.stage.Move',
             Tap: 'ns.tests.xs.pointer.stage.Tap',
             TapEnd: 'ns.tests.xs.pointer.stage.TapEnd',
@@ -32,7 +33,8 @@ xs.define(xs.Class, 'ns.tests.xs.pointer.Test', function (self, imports) {
             tapEnd: imports.stage.TapEnd,
             contextMenu: imports.stage.ContextMenu,
             move: imports.stage.Move,
-            enter: imports.stage.Enter
+            enter: imports.stage.Enter,
+            leave: imports.stage.Leave
         });
     });
 
